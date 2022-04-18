8f492bd809a212897fc7ef18eacd336716e10ef6
klemmy <a.klemm@hotmail.com>
2022-04-18 10:52:37 +1000

    CODE-5 - Improving api typescript packaging
    - Updated some dependencies
    - Updated the README.md's
    - Update version

--- novel-ideas-rest/src/main/java/com/klemmy/novelideas/NovelIdeasApplication.java
+++ novel-ideas-rest/src/main/java/com/klemmy/novelideas/NovelIdeasApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Novel or script structure",
+@OpenAPIDefinition(info = @Info(title = "Novel Ideas",
     description = "This Rest backend is for managing data and information about a novel or book you want to write",
-    version = "1.0", contact = @Contact(email = "a.klemm@hotmail.com", name = "Anthony Klemm"),
+    version = "1.0.0", contact = @Contact(email = "a.klemm@hotmail.com", name = "Klemmy129"),
     license = @License(name = "GNU GPL-3.0 License")))
 @EnableJpaRepositories(basePackages = "com.klemmy.novelideas.jpa")
 public class NovelIdeasApplication {
